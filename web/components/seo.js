ea696086cdb8bf5f2149f019fde57edb1161fe64
Joseph Hamman <jhamman@ucar.edu>
2020-09-01 09:31:21 -0700

    update seo.js

--- web/components/seo.js
+++ web/components/seo.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 
 const Seo = ({ shareCard, shareDescription, shareTitle }) => (
   <Head>
-    <title>carbonplan / research</title>
+    <title>carbonplan / data</title>
     <meta
       name='description'
       content={
         shareDescription
           ? shareDescription
-          : 'Datasets, models, interactives, and commentary on carbon removal and climate solutions.'
+          : "CarbonPlan's public data catalog."
       }
     />
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
@@ -21,35 +21,35 @@ const Seo = ({ shareCard, shareDescription, shareTitle }) => (
       rel='apple-touch-icon'
       href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.png'
     />
-    <meta property='og:title' content='carbonplan / research' />
+    <meta property='og:title' content='carbonplan / data' />
     <meta
       property='og:description'
       content={
         shareDescription
           ? shareDescription
-          : 'Datasets, models, interactives, and commentary on carbon removal and climate solutions.'
+          : "CarbonPlan's public data catalog."
       }
     />
     <meta
       property='og:image'
       content={`https://carbonplan-assets.s3.amazonaws.com/images/social/${
-        shareCard ? shareCard : 'research'
+        shareCard ? shareCard : 'data'
       }.png`}
     />
     <meta property='og:url' content='https://carbonplan.org' />
-    <meta name='twitter:title' content='carbonplan / research' />
+    <meta name='twitter:title' content='carbonplan / data' />
     <meta
       name='twitter:description'
       content={
         shareDescription
           ? shareDescription
-          : 'Datasets, models, interactives, and commentary on carbon removal and climate solutions.'
+          : "CarbonPlan's public data catalog."
       }
     />
     <meta
       name='twitter:image'
       content={`https://carbonplan-assets.s3.amazonaws.com/images/social/${
-        shareCard ? shareCard : 'research'
+        shareCard ? shareCard : 'data'
       }.png`}
     />
     <meta name='twitter:card' content='summary_large_image' />
