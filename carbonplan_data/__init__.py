18eb7eeec74d23f7307f273a879d9f4ef01ee373
Joseph Hamman <jhamman@ucar.edu>
2020-09-29 23:28:23 -0700

    try using environment variables to specify data prefix

--- carbonplan_data/__init__.py
+++ carbonplan_data/__init__.py
@@ -1,10 +1,20 @@
+import os
+import warnings
+
 import intake
 
-MASTER_CATALOG = "https://carbonplan.org/data/api/intake/master.yaml"
+MASTER_CATALOG = "https://raw.githubusercontent.com/carbonplan/data/master/catalogs/master.yaml"
 
 # open master catalog, in the future, we may want to package the catalogs along side the
 # carbonplan-data distribution
 try:
+    _catalog_env_vars = ["CARBONPLAN_DATA"]
+    for v in _catalog_env_vars:
+        if os.getenv(v) is None:
+            warnings.warn(
+                f"{v} environment variable not set, carbonplan.data.cat may not work as expected"
+            )
+
     cat = intake.open_catalog(MASTER_CATALOG)
 except:
     cat = intake.catalog.Catalog()
