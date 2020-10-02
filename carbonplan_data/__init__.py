147819a2bf65869f2bd6efe171d009334594d219
Joseph Hamman <jhamman@ucar.edu>
2020-10-02 13:42:27 -0700

    update catalog test infrastructure

--- carbonplan_data/__init__.py
+++ carbonplan_data/__init__.py
@@ -6,13 +6,19 @@ import intake
 
 CATALOG_DIR_PATH = pathlib.Path(__file__)
 MASTER_CATALOG_PATH = str(CATALOG_DIR_PATH.parent / "catalogs/master.yaml")
+KNOWN_DATA_LOCATIONS = [
+    "https://storage.googleapis.com/carbonplan-data",
+    "https://carbonplan.blob.core.windows.net/carbonplan-data",
+]
 
 # open master catalog
 _catalog_env_vars = ["CARBONPLAN_DATA"]
 for v in _catalog_env_vars:
     if os.getenv(v) is None:
-        warnings.warn(
-            f"{v} environment variable not set, carbonplan.data.cat may not work as expected"
+        msg = (
+            f"{v} environment variable not set, `carbonplan.data.cat` may not work as expected."
+            f"Known data locations include: {KNOWN_DATA_LOCATIONS}."
         )
+        warnings.warn(msg)
 
 cat = intake.open_catalog(MASTER_CATALOG_PATH)
